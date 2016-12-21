35080aa37ed59cf433d6db2dcf9532cab09963c3
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-12-20 20:42:04 -0500

    added comments to functioning active file

--- activate.php
+++ activate.php
@@ -1,17 +1,21 @@
 <?php
 require('includes/config.php');
 
+//gets memberid and active keys from url
 $memberID = trim($_GET['x']);
 $active = trim($_GET['y']);
 
+//checks to make sure the fields arent empty for the user
 if(is_numeric($memberID) && !empty($active)){
 
+//update active column to YES where the values match the ones provided in  array
 	$stmt = $db->prepare("UPDATE members SET active = 'Yes' WHERE memberID = :memberID AND active = :active");
 	$stmt->execute(array(
 		':memberID' => $memberID,
 		':active' => $active
 	));
-
+	//redirect user if row is updated
+	
 	if($stmt->rowCount() == 1){
 
 		header('Location: login.php?action=active');
