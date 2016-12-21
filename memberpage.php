530922f395e957112f325931e40369a549744bb4
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-12-21 01:27:58 -0500

    removed broken prof pic code

--- memberpage.php
+++ memberpage.php
@@ -18,6 +18,37 @@ require('layout/header.php');
 
 </div>
 
+<div class="container">
+
+	<div class="row">
+
+<?php
+$stmt = "SELECT * FROM members WHERE username = '".$_SESSION['username']."'";
+foreach ($db->query($stmt) as $row) {
+
+  echo '<div style="display:table;width:400px;height:60px;">';
+  echo '<div style="padding-center:10px;display:table-cell;height:30px;color:red">Username:' . $row['username'];
+  echo '<br><br>Email Address: ' . $row['email'] . '</div>';
+  echo '</div>';
+}
+?>
+
+
+		</div>
+	</div>
+</div>
+<br>
+<div class="container">
+  <div class="row">
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="myFile">
+        <br><br><br>
+        <center><input type="submit" value="Change User Profile Pic">
+      </form>
+    </div>
+  </div>
+</div>
+
 <?php
 require('layout/footer.php');
 ?>
--- upload.php
+++ upload.php
@@ -1,25 +1,25 @@
 <?php
-	define("UPLOAD_DIR", "afs/cad.njit.edu/u/b/s/bss46/public_html/UPLOADS/");
+	define("UPLOAD_DIR", "afs/cad.njit.edu/u/b/s/bss46/public_html/finalProject/UPLOADS/");
 	if (!empty($_FILES["myFile"])){
 		$myFile = $_FILES["myFile"];
 		if($myFile["error"] !== UPLOAD_ERR_OK){
-			echo"<p>An error occured.</p>";
+			echo'<center><p style="color:red">An error occured.</p><center>';
 			exit;
 		}
+    // ensure only valid characters are used
 		$name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
 		$i = 0;
 		$parts = pathinfo($name);
 		while (file_exists(UPLOAD_DIR . $name)) {
 			$i++;
-			$name = $parts["filename"] . "-" . "." . $parts["extension"];
+			$name = $parts["filename"] . "-" . $i ."." . $parts["extension"];
 		}
 		$success = move_uploaded_file($myFile["tmp_name"], UPLOAD_DIR . $name);
 		if (!success){
-			echo "<p>Unable to save file.</p>";
-			print_r($_FILES);
+			echo '<center><p style="color:red">Unable to save file.</p><center>';
 			exit;
 	}
 	chmod(UPLOAD_DIR . $name, 0644);
-	echo 'Uploaded '.$name. '.';
+  header('Location: memberpage.php')
 	}
 ?>
