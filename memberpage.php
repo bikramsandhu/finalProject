5382d3844104d42f0c37b325034c8fdd1325a404
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-12-20 22:35:35 -0500

    added a comment, not big update

--- memberpage.php
+++ memberpage.php
@@ -1,5 +1,5 @@
 <?php require('includes/config.php');
-
+//checks if logged in, if not then go to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
 
 $title = 'User Profile';
@@ -8,10 +8,9 @@ require('layout/header.php');
 ?>
 <img src="http://archives.njit.edu/vhlib/images/njit_logo.gif" alt="NJIT logo">
 <div class="container">
-
+<! What we see on the member page>
 	<div class="row">
 				<center><h1 style="color:red"> User Profile - Welcome user <?php echo $_SESSION['username']; ?></h1></center><br>
-				<center><h3><a href='logout.php'>Logout</a></h3></center>
 				<center><input type="button" value="Log Out" onclick="window.location.href='logout.php'"/></center>
 		</div>
 	</div>
